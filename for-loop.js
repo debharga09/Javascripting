940e8047700ee3d8bd86649584863698569fa529
Deepika Bhargava <deepibhargava09@gmail.com>
2021-11-13 10:56:52 -0500

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,4 @@
+const total = 0;
+const limit = 10;
+for (const i = 0; i < 10; i++){//log the number 0 through 9
+console.log(total = i)}
---
+++ string-length.js
@@ -0,0 +1,5 @@
+let example = 'iamdeepika';
+var z = example.length;
+console.log(z);
+
+
